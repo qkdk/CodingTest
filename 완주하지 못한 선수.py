86f5a794d552b206655feb0a213a053ca682d2a9
GB <akb033134@gmail.com>
2022-02-15 17:44:14 +0900

    first commit

--- 완주하지 못한 선수.py
+++ 완주하지 못한 선수.py
@@ -11,3 +11,30 @@ def solution(participant, completion):
             answer = participant[i]
             return answer
     return answer
+
+
+# import collections
+
+
+# def solution(participant, completion):
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
+# collection 모듈의 counter 는 리스트 객채의 개수를 dic형태로 반환해준다.
+
+
+# def solution(participant, completion):
+#     answer = ''
+#     temp = 0
+#     dic = {}
+#     for part in participant:
+#         dic[hash(part)] = part
+#         temp += int(hash(part))
+#     for com in completion:
+#         temp -= hash(com)
+#     answer = dic[temp]
+
+#     return answer
+# 해쉬값을 +-로 빈 해쉬값 찾음
+
+
+# 원소의 Insert, Delete, Find ... <- Dic의 효율이 list 보다 좋음(다른 언어에서는 Hash Table)
