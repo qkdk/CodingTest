1b379c3853a1b25796367d8325c78dc1a33716fb
GB <akb033134@gmail.com>
2022-04-12 17:28:57 +0900

    second commit

--- 소수 찾기.py
+++ 소수 찾기.py
@@ -1,12 +1,21 @@
-def solution(n):
-    answer = n-1
-    
-    for i in range(3,n+1):
-        for j in range(2,i):
-            if i%j == 0:
-                answer -= 1
-                break
-              
-    return answer
+import math
 
-solution(10)
+def solution(n):   
+    num_list = [x for x in range(1,n+1)]
+    num_list_answer = []
+
+    temp1 = math.ceil(n**(1/2))
+    for i in range(1,temp1):
+        if num_list[i] != 0:
+            num_list_answer.append(num_list[i])
+            for j in range(i,n,num_list[i]):
+                num_list[j] = 0 
+
+    for k in range(n):
+        if num_list[k] != 0:
+            num_list_answer.append(num_list[k])
+        
+        answer = len(num_list_answer)-1
+    return answer
+    
+# 아리스토테네스의 체 이용
